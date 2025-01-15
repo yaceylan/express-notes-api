a9944c130f57bcb623083303eb1e508b3ac249d8
Yasemin Ceylan <yaceybln@gmail.com>
2025-01-15 21:49:03 +0100

    added html functions

--- index.js
+++ index.js
@@ -17,20 +17,29 @@ app.listen(port, () => {
 app.get('/notes', (req, res) => {
     console.log('Fetching notes:', notes); 
     res.json(notes);
-  })
+})
+  
+app.get('/:id', (req, res) => res.send(`Note ${req.params.id}`));
 app.get("/notes", (req, res) => {
     res.json("notes");
 });
+
 
 app.post("/notes", (req, res) => {
     res.json("notes");
 });
 
+app.post('/', (req, res) => res.send('Creating a new note'));
+
 app.put("/notes", (req, res) => {
     res.json("notes");
 });
 
+app.put('/:id', (req, res) => res.send(`Updating note ${req.params.id}`));
+
 app.delete("/notes", (req, res) => {    
     res.json("notes");
 });
+
+app.delete('/:id', (req, res) => res.send(`Deleting note ${req.params.id}`));
 
